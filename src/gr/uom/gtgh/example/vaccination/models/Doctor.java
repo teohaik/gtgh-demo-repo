3f78fcb4e818443c023af5dbc9608780b2ae3fb0
Theodore Chaikalis <teohaik@gmail.com>
2022-06-02 15:12:53 +0300

    Update Doctor.java
    add constructor

--- src/gr/uom/gtgh/example/vaccination/models/Doctor.java
+++ src/gr/uom/gtgh/example/vaccination/models/Doctor.java
@@ -4,6 +4,12 @@ public class Doctor {
 
     private String name;
     private String amka;
+    
+    
+    public Doctor(){}
+    
+    
+    //we need to add getters and setters as well...
 
 
 }
